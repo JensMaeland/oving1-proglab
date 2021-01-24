3450b4350c213fa5c9d1cf0e31c9b2dcd789e649
JensMaeland <jens_mae@hotmail.com>
2021-01-24 13:42:47 +0100

    Initial Commit

---
+++ oving1-pongTests/oving1_pongTests.swift
@@ -0,0 +1,33 @@
+//
+//  oving1_pongTests.swift
+//  oving1-pongTests
+//
+//  Created by Jens Mæland on 24/01/2021.
+//
+
+import XCTest
+@testable import oving1_pong
+
+class oving1_pongTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
